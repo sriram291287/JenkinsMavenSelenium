5bf2986030768b90b7ae4b56e1d37140c1b8bbdc
sriram291287 <sriram291287@gmail.com>
2017-06-20 22:17:02 +0530

    1st Commit

---
+++ src/JenkinsMavenTestNG.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+public class JenkinsMavenTestNG {
+	
+	@Test
+	public void sample(){
+		System.setProperty("webdriver.gecko.driver","E:\\selenium\\eclipse\\drivers\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https:\\google.co.in");
+		String appTitle = driver.getTitle();
+		System.out.println("Application title is :: "+appTitle);
+		driver.quit();
+	}
+
+}
